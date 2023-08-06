93f1f1dcf0dff5865d5ddb9873d5c14f00eb4679
BogdanGuyAvrahamCom <bogdan@guy-avraham.com>
2023-08-06 16:24:31 +0300

    🗿 wth: testing on a server (because of webhook thingy)

--- bot.ts
+++ bot.ts
@@ -11,7 +11,10 @@ const bot = new Bot(BOT_TOKEN); // <-- put your bot token between the ""
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
 
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
+bot.on('message', (ctx) => {
+  console.log(JSON.stringify(ctx));
+  return ctx.reply(JSON.stringify(ctx));
+});
 
 const handleUpdates = webhookCallback(bot, 'oak');
 
