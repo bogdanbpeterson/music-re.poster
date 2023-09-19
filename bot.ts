75f3c86d4f95814c2de1084ffeef7dbfedc932af
Bogdan B Peterson <github@bogdanbpeterson.com>
2023-09-19 13:34:17 +0300

    📋 feat: adds check for user (currently only one)

--- bot.ts
+++ bot.ts
@@ -4,14 +4,16 @@ export const raise = (error: string): never => {
   throw new Error(error);
 };
 
-const MUSIC_CHANNEL = Deno.env.get('MUSIC_CHANNEL') ?? raise('No music channel to send to');
 const BOT_TOKEN = Deno.env.get('BOT_TOKEN') ?? raise('No BOT_TOKEN variable');
+const MUSIC_CHANNEL = Deno.env.get('MUSIC_CHANNEL') ?? raise('No MUSIC_CHANNEL variable');
+const ME = Deno.env.get('ME') ?? raise('No ME variable');
 
-const bot = new Bot(BOT_TOKEN); // <-- put your bot token between the ""
+const bot = new Bot(BOT_TOKEN);
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
 
 bot.on(':audio', (ctx) => {
+  if (ctx.chat.id.toString() !== ME) return ctx.reply('Something wrong, reporting...');
   if (!ctx.msg.audio?.file_id) return ctx.reply('Something wrong, reporting...');
   void ctx.api.sendAudio(MUSIC_CHANNEL, ctx.msg.audio.file_id, {
     caption: '[Music: Reborn](https://t.me/the_ankh_music)',
