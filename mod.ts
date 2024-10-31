e8c4031ec218ea0ec49ecd0edf7a6212dd07d455
Bogdan B Peterson <github@bogdanbpeterson.com>
2024-10-31 21:44:25 +0200

    fix: add selfhealing

--- mod.ts
+++ mod.ts
@@ -7,7 +7,8 @@ const router = new Router();
 router
   .get('/', (context) => {
     context.response.body = 'Welcome Music Re:Poster!';
-  }).post(`/${bot.token}`, handleUpdates);
+  })
+  .post(`/${bot.token}`, handleUpdates);
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
@@ -15,4 +16,9 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log('run');
+console.log(
+  await (
+    await fetch(`https://api.telegram.org/bot${bot.token}/setWebhook?url=${Deno.env.get('PUBLIC_URL')}/${bot.token}`)
+  ).text(),
+);
 await app.listen({ port: 8000 });
